f81f72c89cbbcaeacf12954532f7c2a880410bf3
NikitaProkhoda <s31524@pjwstk.edu.pl>
2025-03-03 18:23:04 +0100

    Modification 2

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1 Example");
+Console.WriteLine("Modification 2 Example");
